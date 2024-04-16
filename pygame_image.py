17b2d15b84f04acc85c63adc398518320c3832e8
C0B23156 <c0b231568f@edu.teu.ac.jp>
2024-04-16 17:45:45 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -20,6 +20,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        kk_rct.move_ip([-1,0])
         if key_lst[pg.K_UP]:
             kk_rct.move_ip([0, -1])
         if key_lst[pg.K_DOWN]:
@@ -27,7 +28,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip([-1, 0])
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip([1, 0])
+            kk_rct.move_ip([2, 0])
         x=tmr%3200    
         screen.blit(bg_img, [-x, 0]) #練習６
         screen.blit(bg_img2, [-x+1600, 0] ) #練習7-1
